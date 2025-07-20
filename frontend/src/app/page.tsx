dd620c29a487cec7e61d31cf63042893e22fa616
WeiShiuan <zenfonlee@gmail.com>
2025-07-20 20:27:08 +0800

    use shadcn instead of heroui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { ThemeToggle } from '../components/shared/ThemeToggle';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      <ThemeToggle></ThemeToggle>
+    </main>
+  );
 }
