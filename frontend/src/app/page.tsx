946cbf201a54567370158059e376e35fecd78869
WeiShiuan <zenfonlee@gmail.com>
2025-07-31 23:56:08 +0800

    add signup page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,6 @@ import { signIn } from '@/auth';
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
       <form
         action={async () => {
           'use server';
