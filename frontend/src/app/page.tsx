08bf0e08a17b7423da35907d4565ba3cd72e7581
WeiShiuan <zenfonlee@gmail.com>
2025-07-31 23:52:56 +0800

    delete unused code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
 import { signIn } from '@/auth';
-import { ThemeToggle } from '../components/shared/ThemeToggle';
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
-      <ThemeToggle></ThemeToggle>
       <form
         action={async () => {
           'use server';
